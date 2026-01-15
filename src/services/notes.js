edcd5f3d1c94d7bc00bf56601dadfc85343e898b
joewaine <joe.waine@gmail.com>
2026-01-15 22:50:03 +0100

    g

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL
+
 // const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
